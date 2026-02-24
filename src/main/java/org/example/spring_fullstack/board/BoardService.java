baa2349d40603d66fca4c7af61455e69e42ec0fa
saralove20 <saralove20@naver.com>
2026-02-24 11:27:48 +0900

    [Feat] 게시글 상세 조회 기능 구현

--- src/main/java/org/example/spring_fullstack/board/BoardService.java
+++ src/main/java/org/example/spring_fullstack/board/BoardService.java
@@ -10,11 +10,23 @@ import org.springframework.stereotype.Service;
 public class BoardService {
     private final BoardRepository boardRepository;
 
+    // 게시글 작성
     public BoardDto.RegisterRes register(BoardDto.RegisterReq dto) {
         // 1. 요청 DTO를 Entity로 변환하여 저장
         Board board = boardRepository.save(dto.toEntity());
 
         // 2. 저장된 Entity를 응답 DTO로 변환하여 반환
         return BoardDto.RegisterRes.from(board);
+    }
+
+    // 게시글 상세 조회
+    public BoardDto.ReadRes read(Long idx) {
+        // 1. 게시글 조회 결과를 Entity에 저장
+        Board board = boardRepository.findById(idx).orElseThrow(
+                () -> new RuntimeException()
+        );
+
+        // 2. 조회 결과 Entity를 응답 DTO로 변환하여 반환
+        return BoardDto.ReadRes.from(board);
     }
 }
