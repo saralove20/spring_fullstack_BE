bfc66a511be4b3383dd563d4683f7bbc5e7a3c42
saralove20 <saralove20@naver.com>
2026-02-25 20:44:11 +0900

    [Chore] git 추적 안되는 파일들 전부 add 하고 깃허브에 push, 다른 컴퓨터에서 클론해서 작업하려면 해당 파일들 필요

---
+++ src/main/java/org/example/spring_fullstack/SpringFullstackApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFullstackApplication.class, args);
+    }
+
+}
